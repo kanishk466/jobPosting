9010e2fff67916d22635104f17f52e98c9aadcdd
Kanishk <kanishkas466@gmail.com>
2025-03-10 21:36:17 +0530

    update navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="https://forceonecorporation.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Forceonecorporation</span>
         </a>
 
         {/* Mobile Menu Button */}
